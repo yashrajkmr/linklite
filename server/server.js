244b679b102d7b03381a76fd6c44a71377706d89
yashrajkmr <yashrajkmr01@gmail.com>
2025-12-24 23:18:34 +0530

    allow all cors

--- server/server.js
+++ server/server.js
@@ -10,10 +10,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: 'http://localhost:3000', 
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
